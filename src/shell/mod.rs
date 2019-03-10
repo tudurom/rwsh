8ca4cc59d2c130c65fb8f3a74585a7c6ea3342b1
Tudor Roman <tudurom@gmail.com>
2019-03-10 21:01:20 +0200

    cd and home expansion

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,5 +1,8 @@
 use crate::parser::parse_line;
-use std::io::{self, stdin, stdout, Write};
+use dirs;
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::{Path, PathBuf};
 use std::process::{exit, Command};
 
 #[derive(Default)]
@@ -25,23 +28,56 @@ impl Shell {
                 continue;
             }
             let args = parts;
-            match self.run_command(command.unwrap(), args) {
-                Err(ref error) if error.kind() == io::ErrorKind::NotFound => {
-                    eprintln!("Command not found");
-                }
-                Err(error) => {
-                    panic!(error);
-                }
-                _ => {}
+            if let Err(error) = self.run_command(command.unwrap(), args) {
+                eprintln!("{}", error);
             }
         }
     }
-    fn run_command<'a, I>(&self, command: &str, args: I) -> io::Result<()>
+    fn do_cd<'a, I>(&self, mut args: I) -> Result<(), String>
     where
         I: Iterator<Item = &'a str>,
     {
-        let mut child = Command::new(command).args(args).spawn()?;
-        child.wait()?;
-        Ok(())
+        if let Some(arg) = args.next() {
+            let path = expand_home(Path::new(arg));
+            match env::set_current_dir(path) {
+                Err(error) => Err(format!("cd: {}", error)),
+                _ => Ok(()),
+            }
+        } else {
+            Err(String::from("cd needs an argument"))
+        }
     }
+    fn run_command<'a, I>(&self, command: &str, args: I) -> Result<(), String>
+    where
+        I: Iterator<Item = &'a str>,
+    {
+        match command {
+            "cd" => self.do_cd(args),
+            _ => match Command::new(command).args(args).spawn() {
+                Ok(mut child) => {
+                    if let Err(error) = child.wait() {
+                        return Err(format!("rwsh: {}", error));
+                    }
+                    Ok(())
+                }
+                Err(error) => Err(format!("rwsh: {}", error)),
+            },
+        }
+    }
+}
+
+fn expand_home<P: AsRef<Path>>(path: P) -> PathBuf {
+    let mut new_path = PathBuf::new();
+    let mut it = path.as_ref().iter().peekable();
+
+    if let Some(p) = it.peek() {
+        if *p == "~" {
+            new_path.push(dirs::home_dir().unwrap());
+            it.next();
+        }
+    }
+    for p in it {
+        new_path.push(p);
+    }
+    new_path
 }
