b73bd180ef7be4e536e8ac413ad382b502af4cb9
Tudor Roman <tudurom@gmail.com>
2019-06-03 09:50:20 +0300

    run_examples.sh: remove temp file

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+finish() {
+	rm "$temp"
+}
+
 cargo build
 
 temp="$(mktemp /tmp/rwsh.XXXX)"
-for example in *.rwsh; do 
+trap finish EXIT
+for example in *.rwsh; do
     echo $example
     ../target/debug/rwsh "$example" > "$temp"
     if ! diff "$temp" "${example%.rwsh}.out" > /dev/null; then
