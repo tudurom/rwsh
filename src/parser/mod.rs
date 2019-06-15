8e1b34474682a0fddb1a2abb89251e4936eb2e85
Tudor Roman <tudurom@gmail.com>
2019-06-15 11:54:46 +0300

    added license

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,20 @@
+/* Copyright (C) 2019 Tudor-Ioan Roman
+ *
+ * This file is part of the Really Weird Shell, also known as RWSH.
+ *
+ * RWSH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RWSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RWSH. If not, see <http://www.gnu.org/licenses/>.
+ */
 //! The parsers and lexers of the `rwsh` scripting language and its SRE sublanguage.
 pub mod lex;
 pub mod misc;
